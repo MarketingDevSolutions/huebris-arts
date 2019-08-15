4a0fd666504603cad58e9f58d5dc064cb61a5025
Daniel Esteves <estevesd8@gmail.com>
2019-08-15 12:40:35 -0400

    service worker initial

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // server.js
 const next = require('next')
-const { join } = require('path')
 const routes = require('./routes')
 const app = next({ dev: false })
 const handler = routes.getRequestHandler(app)
@@ -9,20 +8,6 @@ const port = process.env.PORT || 3000
 
 // Without express
 const { createServer } = require('http')
-app.prepare()
-  .then(() => {
-    createServer((req, res) => {
-      const parsedUrl = new URL(req.url, true)
-      const { pathname } = parsedUrl
-
-      // handle GET request to /service-worker.js
-      if (pathname === '/service-worker.js') {
-        const filePath = join(__dirname, 'build', pathname)
-
-        app.serveStatic(req, res, filePath)
-      } else {
-        handler(req, res, parsedUrl)
-      }
-    })
-      .listen(port)
-  })
+app.prepare().then(() => {
+  createServer(handler).listen(port)
+})
