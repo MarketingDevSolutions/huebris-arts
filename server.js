2acda6b80edaed0fe840f77a9d57907134c8004d
Daniel Esteves <estevesd8@gmail.com>
2019-08-15 12:07:51 -0400

    serving with new URL

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-// server.js
-const next = require('next')
-const routes = require('./routes')
-const { join } = require('path')
-const url = require('url').URL
-const app = next({ dev: false })
-const handler = routes.getRequestHandler(app)
-
-const port = process.env.PORT || 3000
-
-// Without express
 const { createServer } = require('http')
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = url(req.url, true)
-    const { pathname } = parsedUrl
+const { join } = require('path')
+const next = require('next')
 
-    // handle GET request to /service-worker.js
-    if (pathname === '/service-worker.js') {
-      const filePath = join(__dirname, '.next', pathname)
+const app = next({ dev: process.env.NODE_ENV !== 'production' })
+const handle = app.getRequestHandler()
 
-      app.serveStatic(req, res, filePath)
-    } else {
-      handler(req, res, parsedUrl)
-    }
+app.prepare()
+  .then(() => {
+    createServer((req, res) => {
+      const parsedUrl = new URL(req.url, true)
+      const { pathname } = parsedUrl
+
+      // handle GET request to /service-worker.js
+      if (pathname === '/service-worker.js') {
+        const filePath = join(__dirname, '.next', pathname)
+
+        app.serveStatic(req, res, filePath)
+      } else {
+        handle(req, res, parsedUrl)
+      }
+    })
+      .listen(3000, () => {
+        console.log(`> Ready on http://localhost:${3000}`)
+      })
   })
-    .listen(port)
-})
